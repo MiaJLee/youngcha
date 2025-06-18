c8c72473672b309a5e57d2e732dd3c485e11fee6
MiaJLee <angella555@naver.com>
2025-06-18 15:54:18 +0900

    feat: 빌드, 배포 대응

---
+++ vite.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import webExtension from 'vite-plugin-web-extension'
+
+export default defineConfig({
+	plugins: [webExtension({ manifest: 'manifest.json' })],
+	build: {
+		outDir: 'dist',
+		emptyOutDir: true,
+		sourcemap: false,
+		minify: true,
+	},
+})
